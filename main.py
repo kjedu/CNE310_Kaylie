8089f41b7f79119541ebc7ae62cb50110618c390
kjedu <kjvelazquezbecerra@student.rtc.edu>
2024-10-16 17:52:14 -0700

    Last CNE330 Assignment

---
+++ main.py
@@ -0,0 +1,46 @@
+def verse():
+    print('Esta noche hay Luna llena')
+    print('Cargo energías buenas')
+    print('Pero hay una pena que no me deja ser')
+    print('Ay, esa mujer')
+    print("")
+def pre_chorus():
+    print('No la veo por abstinencia')
+    print('De tenerla cerca, me puse una meta')
+    print('De lejos es mejor')
+    print('No por mí, esto es por los dos')
+    print("")
+def chorus():
+    print('Y nado entre lagunas de mi mente')
+    print('Que se hacen por saber qué se siente')
+    print('Volver a besar tu frente')
+    print('Tal vez en otra galaxia sí fue diferente')
+    print('Tal vez ahí sí se nos dio lo que quisimos siempre, eh')
+    print("")
+def post_chorus1():
+    print('Na, na, na-na')
+    print('Na, na, na, na, na')
+    print('Na, na, na-na')
+    print("")
+def post_chorus2():
+    print('Na, na, na-na, na-na')
+    print('Na, na, na-na, na, na, na')
+    print('Na-na, na, na-na')
+    print('Eh')
+    print("")
+def outro():
+    print('Y así suena jasiel nuñez')
+    print('Y pura doble p')
+    print('Chau')
+    print("")
+def play_song():
+    verse()
+    pre_chorus()
+    chorus()
+    post_chorus1()
+    pre_chorus()
+    chorus()
+    post_chorus2()
+    outro()
+
+play_song()
